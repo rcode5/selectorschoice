ba2910a77e2cd05fc3ee56144d83af2218e6df05
Mr Rogers <jon@bunnymatic.com>
2013-03-04 16:32:07 -0500

    * fix specs for willpaginate

--- spec/views/welcome/index.haml_spec.rb
+++ spec/views/welcome/index.haml_spec.rb
@@ -3,7 +3,9 @@ require 'spec_helper'
 describe 'welcome/index' do
   before do
     5.times { |x| FactoryGirl.create :track, published: ((x%2) == 0) }
-    assign(:tracks, Track.published)
+    tracks = Track.published.all
+    tracks.stub!(:total_pages).and_return(0)
+    assign(:tracks, tracks)
     render
   end
   it 'renders published tracks' do
