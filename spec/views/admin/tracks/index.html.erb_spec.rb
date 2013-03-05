621e88000925b514312540737fb2d73d706bd05f
Mr Rogers <jon@bunnymatic.com>
2013-03-04 21:40:07 -0500

    * fix specs

--- spec/views/admin/tracks/index.html.erb_spec.rb
+++ spec/views/admin/tracks/index.html.erb_spec.rb
@@ -2,11 +2,15 @@ require 'spec_helper'
 
 describe "admin/tracks/index" do
   before(:each) do
-    @tracks = assign(:tracks, [FactoryGirl.create(Track), FactoryGirl.create(Track, published: false)])
+    published = [FactoryGirl.create(Track)]
+    unpublished = [FactoryGirl.create(Track, published: false)]
+    @tracks = published + unpublished
+    assign(:published, published)
+    assign(:unpublished, unpublished)
+    render
   end
 
   it "renders a list of tracks" do
-    render
     @tracks.each do |track|
       assert_select "tr td.recorded_on", :text => track.recorded_on.strftime("%m %d %Y")
       assert_select "tr .info .title", track.pretty_title
