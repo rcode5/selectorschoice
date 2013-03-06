6b804e4eb8bc32695e74a007a2c1c27e6f183df0
Mr Rogers <jon@bunnymatic.com>
2013-03-05 21:00:29 -0800

    * fix specs

--- spec/views/admin/tracks/index.html.erb_spec.rb
+++ spec/views/admin/tracks/index.html.erb_spec.rb
@@ -15,7 +15,7 @@ describe "admin/tracks/index" do
       assert_select "tr td.recorded_on", :text => track.recorded_on.strftime("%m %d %Y")
       assert_select "tr .info .title", track.pretty_title
       assert_select "tr .info .author", :match => /#{track.author}/
-      assert_select "tr .info .url input.url"
+      assert_select "tr .track .url input.url"
       assert_select "tr .edit-controls .icon-edit"
       assert_select "tr .edit-controls .icon-show"
       assert_select "tr .edit-controls .icon-delete"
