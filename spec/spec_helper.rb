4984f2f4c82f170a0987dbebbd289fe3118f78e3
Mr Rogers <jon@bunnymatic.com>
2016-09-21 19:54:15 -0700

    remove rspce/autorun require

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,16 +5,15 @@ SimpleCov.start
 require File.expand_path('../../config/boot', __FILE__)
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
 
 Dir[Rails.root.join("spec/{factories,support}/**/*.rb")].each {|f| require f}
 
 #Capybara.javascript_driver = :webkit
 
 RSpec.configure do |config|
-  
+
   config.mock_with :rspec
-  
+
     config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   config.use_transactional_fixtures = true
