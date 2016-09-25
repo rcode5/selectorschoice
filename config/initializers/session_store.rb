af98dc7ac507caaaa82b1d24da26ee89e80e96ed
Mr Rogers <jon@bunnymatic.com>
2016-09-25 12:22:18 -0700

    bump to rails 4.2

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-SelectorsChoice::Application.config.session_store :cookie_store, key: '_selectors_choice_session'
+Rails.application.config.session_store :cookie_store, key: '_selectors_choice_session'
