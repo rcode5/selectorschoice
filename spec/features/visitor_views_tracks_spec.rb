85c14c730ab46279258156dcc8766fd57d2bad5e
Jon Rogers <jon@carbonfive.com>
2024-09-02 10:16:40 -0700

    Add mobile support and search
    Problem
    ------
    
    we had a somewhat responsive design but the background image was pretty bad
    
    Additionally we have a search page but never surfaced it.
    
    solution
    ------
    
    Adding links to search page and fix background image css to be more responsive.

--- spec/features/visitor_views_tracks_spec.rb
+++ spec/features/visitor_views_tracks_spec.rb
@@ -3,7 +3,14 @@
 require 'rails_helper'
 
 feature 'Visitor' do
-  let(:tracks) { FactoryBot.create_list(:track, 3, :published) }
+  let(:tracks) do
+    [
+      FactoryBot.create(:track, :published, title: 'the first mix today',
+playlist: "** bootylicious - Destiny's Child"),
+      FactoryBot.create(:track, :published, title: 'the second mix today', playlist: 'james brown here'),
+      FactoryBot.create(:track, :published, title: 'the third mix today'),
+    ]
+  end
   let(:unpublished_tracks) { FactoryBot.create_list(:track, 1) }
 
   before do
@@ -16,5 +23,15 @@ feature 'Visitor' do
     expect(page).to have_css('.tracks .track', count: 3)
     click_on tracks.first.title
     expect(page).to have_css('.track', count: 1)
+  end
+
+  scenario 'can search', js: true do
+    visit root_path
+    expect(page).to have_css('.tracks .track', count: 3)
+    click_on 'search'
+    fill_in 'search', with: "bootylicious\n"
+    expect(page).to have_content 'the first mix today'
+    fill_in 'search', with: "james brown\n"
+    expect(page).to have_content 'the second mix today'
   end
 end
